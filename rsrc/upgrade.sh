b7b49fc792eb8e5cd254275292464faa2b245647
DarthWound <darthwound@live.com>
2018-01-04 03:47:07 +0100

    Update upgrade.sh
    Add reboot dialog (requires SystemD)

--- rsrc/upgrade.sh
+++ rsrc/upgrade.sh
@@ -4,13 +4,31 @@
 su -c 'zypper ref && zypper dup --no-allow-vendor-change --no-recommends'
 
 # openSUSE Leap
-#su -c 'zypper ref && zypper up --no-recommends'
+# su -c 'zypper ref && zypper up --no-recommends'
 
 # Fedora
-#su -c 'dnf --refresh upgrade'
+# su -c 'dnf --refresh upgrade'
 
 # Ubuntu/Debian
-#su -c 'apt update && apt upgrade'
+# su -c 'apt update && apt upgrade'
 
 # Arch
-#su -c 'pacman -Syu'
+# su -c 'pacman -Syu'
+
+# Ask for reboot in french (requires SystemD)
+read -p "Redémarrer l'ordinateur ? [O/n]" REP
+if [ "$REP" = "O" ] || [ "$a" = "o" ]
+then
+systemctl reboot;
+else
+exit 0;
+fi
+
+# Ask for reboot in english (requires SystemD)
+# read -p "Reboot computer? [Y/n]" REP
+# if [ "$REP" = "Y" ] || [ "$a" = "y" ]
+# then
+# systemctl reboot;
+# else
+# exit 0;
+# fi
