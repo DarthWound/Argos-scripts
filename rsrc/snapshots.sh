d920ed8ba3a688d67c74c12449de20df94de0694
DarthWound <darthwound@live.com>
2018-01-04 03:44:23 +0100

    Add snapshots.sh
    Check if a new openSUSE Tumbleweed snapshot is available.

---
+++ rsrc/snapshots.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+TWSSA=$(wget -qO - "http://download.opensuse.org/tumbleweed/repo/oss/media.1/products" | grep -oP " \K\d+")
+TWSSC=$(lsb_release -sr)
+
+# FRENCH
+if [ "$TWSSA" == "$TWSSC" ] ; then
+echo "Aucun nouveau snapshot disponible."
+elif [ "$TWSSA" != "$TWSSC" ] ; then
+echo "Snapshot \"$TWSSA\" disponible !"
+fi
+
+# ENGLISH
+#if [ "$TWSSA" == "$TWSSC" ] ; then
+#echo "No new snapshot available."
+#elif [ "$TWSSA" != "$TWSSC" ] ; then
+#echo "Snapshot \"$TWSSA\" available!"
+#fi
