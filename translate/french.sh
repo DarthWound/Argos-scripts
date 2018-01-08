677a9991a8dda6c3f4392b25b4024a8e47b9ecb6
DarthWound <darthwound@live.com>
2018-01-08 06:47:14 +0100

    Update french.sh

--- translate/french.sh
+++ translate/french.sh
@@ -7,17 +7,18 @@ printf "Traduction des scripts Argos en français...\n"
 
 cat > $DIR/scripts.30m.sh << "EOF"
 #!/usr/bin/env bash
-
-# Content of this script is updated every 30 minutes (rename "30m" part in scripts.30m.sh to change that)
+# Argos-DWS: argos scripts by darthwound
+# Content is updated every 30 minutes (rename "30m" part in "scripts.30m.sh" to change that)
 
 DIR=$(dirname "$0")/rsrc
 DIST=$(lsb_release -sd | tr -d '"')\ \($(lsb_release -sr)\)
-TWSS=$($DIR/snapshots.sh) # openSUSE Tumbleweed only
+# following variables are for openSUSE Tumbleweed only (new snapshot notification and details)
+TWSS=$($DIR/snapshots.sh)
+TWSSA=$(wget -qO - "http://download.opensuse.org/tumbleweed/repo/oss/media.1/products" | grep -oP " \K\d+")
 
 echo "$DIST"
 echo "---"
-echo "<small><i><b>$TWSS</b></i></small>" # openSUSE Tumbleweed only
-echo "---"
+echo "<small><i>$TWSS</i></small> | href='https://download.opensuse.org/tumbleweed/iso/Changes.$TWSSA.txt' color=#8e8e80" # openSUSE Tumbleweed only
 echo "<b>Mettre à jour le système</b> | iconName=view-refresh-symbolic | bash='screen $DIR/upgrade.sh && exit 0' terminal=true color=#35b9ab" # edit upgrade.sh for distro
 echo "<b>Lancer la console YaST</b> | iconName=system-run-symbolic | bash='$DIR/yast.sh && exit 0' terminal=true color=#73ba25" # openSUSE (Leap and Tumbleweed) only
 echo "---"
@@ -26,7 +27,7 @@ echo "Entrer dans la matrice | bash='gnome-terminal --full-screen -- python3 $DI
 echo "---"
 echo "Autres"
 echo "-- Ouvrir le dossier de scripts | bash='xdg-open $DIR' terminal=false"
-echo "-- Éditer le CSS gnome-shell | bash='xdg-open ~/.themes/Adwaita-DWS/gnome-shell/gnome-shell.css' terminal=false" # only if you use my custom gnome-shell theme
+#echo "-- Éditer le CSS gnome-shell | bash='xdg-open ~/.themes/Adwaita-DWS/gnome-shell/gnome-shell.css' terminal=false" # only if you use my custom gnome-shell theme
 EOF
 
 cat > $DIR/rsrc/snapshots.sh << "EOF"
@@ -36,7 +37,7 @@ TWSSA=$(wget -qO - "http://download.opensuse.org/tumbleweed/repo/oss/media.1/pro
 TWSSC=$(lsb_release -sr)
 
 if [ "$TWSSA" == "$TWSSC" ] ; then
-echo "Aucun nouveau snapshot disponible."
+echo "\033[1mAucun nouveau snapshot disponible.\033[0m"
 elif [ "$TWSSA" != "$TWSSC" ] ; then
 echo "\033[1mSnapshot \"$TWSSA\" disponible !\033[0m\nCliquer ici pour consulter le changelog."
 notify-send -i software-update-available-symbolic "Snapshot \"$TWSSA\" disponible !"
