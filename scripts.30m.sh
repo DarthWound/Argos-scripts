99c84fa3925660f25cb2c7fd4e4ca1d3440798a8
DarthWound <darthwound@live.com>
2018-01-08 04:30:21 +0100

    Update scripts.30m.sh
    Add ability to read Tumbleweed latest changelogs

--- scripts.30m.sh
+++ scripts.30m.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
-
-# Content of this script is updated every 30 minutes (rename "30m" part in scripts.30m.sh to change that)
+# Argos-DWS: argos scripts by darthwound
+# Content is updated every 30 minutes (rename "30m" part in "scripts.30m.sh" to change that)
 
 DIR=$(dirname "$0")/rsrc
 DIST=$(lsb_release -sd | tr -d '"')\ \($(lsb_release -sr)\)
-TWSS=$($DIR/snapshots.sh) # openSUSE Tumbleweed only
+# following variables are for openSUSE Tumbleweed only (new snapshot notification and details)
+TWSS=$($DIR/snapshots.sh)
+TWSSA=$(wget -qO - "http://download.opensuse.org/tumbleweed/repo/oss/media.1/products" | grep -oP " \K\d+")
 
 echo "$DIST"
 echo "---"
-echo "<small><i><b>$TWSS</b></i></small>" # openSUSE Tumbleweed only
-echo "---"
+echo "<small><i>$TWSS</i></small> | href='https://download.opensuse.org/tumbleweed/iso/Changes.$TWSSA.txt' color=#8e8e80" # openSUSE Tumbleweed only
 echo "<b>Mettre à jour le système</b> | iconName=view-refresh-symbolic | bash='screen $DIR/upgrade.sh && exit 0' terminal=true color=#35b9ab" # edit upgrade.sh for distro
 echo "<b>Lancer la console YaST</b> | iconName=system-run-symbolic | bash='$DIR/yast.sh && exit 0' terminal=true color=#73ba25" # openSUSE (Leap and Tumbleweed) only
 echo "---"
@@ -18,4 +19,4 @@ echo "Entrer dans la matrice | bash='gnome-terminal --full-screen -- python3 $DI
 echo "---"
 echo "Autres"
 echo "-- Ouvrir le dossier de scripts | bash='xdg-open $DIR' terminal=false"
-echo "-- Éditer le CSS gnome-shell | bash='xdg-open ~/.themes/Adwaita-DWS/gnome-shell/gnome-shell.css' terminal=false" # only if you use my custom gnome-shell theme
+#echo "-- Éditer le CSS gnome-shell | bash='xdg-open ~/.themes/Adwaita-DWS/gnome-shell/gnome-shell.css' terminal=false" # only if you use my custom gnome-shell theme
