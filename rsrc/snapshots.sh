431bec386b17bb2f71adff575c964356a8b27e55
DarthWound <darthwound@live.com>
2018-01-18 13:39:37 +0100

    Update snapshots.sh

--- rsrc/snapshots.sh
+++ rsrc/snapshots.sh
@@ -4,7 +4,7 @@ TWSSA=$(wget -qO - "http://download.opensuse.org/tumbleweed/repo/oss/media.1/pro
 TWSSC=$(lsb_release -sr)
 
 if [ "$TWSSA" == "$TWSSC" ] ; then
-echo "\033[1mAucun nouveau snapshot disponible.\033[0m"
+echo "\033[1mAucun nouveau snapshot disponible.\033[0m\nVersion actuelle : \"$TWSSC\"."
 elif [ "$TWSSA" != "$TWSSC" ] ; then
 echo "\033[1mSnapshot \"$TWSSA\" disponible !\033[0m\nCliquer ici pour consulter le changelog."
 notify-send -i software-update-available-symbolic "Snapshot \"$TWSSA\" disponible !"
