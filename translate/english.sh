3bc78a1206cc8c31fd8b4fe274d74fab5055fdad
DarthWound <darthwound@live.com>
2018-01-18 13:39:11 +0100

    Update english.sh

--- translate/english.sh
+++ translate/english.sh
@@ -11,24 +11,23 @@ cat > $DIR/scripts.30m.sh << "EOF"
 # Content is updated every 30 minutes (rename "30m" part in "scripts.30m.sh" to change that)
 
 DIR=$(dirname "$0")/rsrc
-DIST=$(lsb_release -sd | tr -d '"')\ \($(lsb_release -sr)\)
+# run "lsb_release --help" to change this if it doesn't fit your distro
+DIST=$(lsb_release -sd | tr -d '"')
 # following variables are for openSUSE Tumbleweed only (new snapshot notification and details)
 TWSS=$($DIR/snapshots.sh)
 TWSSA=$(wget -qO - "http://download.opensuse.org/tumbleweed/repo/oss/media.1/products" | grep -oP " \K\d+")
 
 echo "$DIST"
 echo "---"
-echo "<small><i>$TWSS</i></small> | href='https://download.opensuse.org/tumbleweed/iso/Changes.$TWSSA.txt' color=#8e8e80" # openSUSE Tumbleweed only
-echo "---"
-echo "<b>System update</b> | iconName=view-refresh-symbolic | bash='screen $DIR/upgrade.sh && exit 0' terminal=true color=#35b9ab" # edit upgrade.sh for distro
 echo "<b>Run YaST console</b> | iconName=system-run-symbolic | bash='$DIR/yast.sh && exit 0' terminal=true color=#73ba25" # openSUSE (Leap and Tumbleweed) only
+echo "<b>System update</b> | iconName=view-refresh-symbolic | bash='screen $DIR/upgrade.sh && exit 0' terminal=true color=#35b9ab" # edit upgrade.sh for distro
+echo "<small><i>$TWSS</i></small> | href='https://download.opensuse.org/tumbleweed/iso/Changes.$TWSSA.txt' color=#8e8e80" # openSUSE Tumbleweed only
 echo "---"
 echo "Toggle light/dark theme | bash='$DIR/daynight.sh' terminal=false" # edit daynight.sh for Adapta, Arc, Geeko, or Materia - request other themes on my GitHub
 echo "Enter the matrix | bash='gnome-terminal --full-screen -- python3 $DIR/matrix-curses.py' terminal=false"
 echo "---"
 echo "Other"
 echo "-- Open scripts folder | bash='xdg-open $DIR' terminal=false"
-#echo "-- Edit gnome-shell CSS | bash='xdg-open ~/.themes/Adwaita-DWS/gnome-shell/gnome-shell.css' terminal=false" # only if you use my custom gnome-shell theme
 EOF
 
 cat > $DIR/rsrc/snapshots.sh << "EOF"
@@ -38,9 +37,9 @@ TWSSA=$(wget -qO - "http://download.opensuse.org/tumbleweed/repo/oss/media.1/pro
 TWSSC=$(lsb_release -sr)
 
 if [ "$TWSSA" == "$TWSSC" ] ; then
-echo "\033[1mNo new snapshot available.\033[0m"
+echo "\033[1mNo new snapshot available.\033[0m\nCurrent version: \"$TWSSC\"."
 elif [ "$TWSSA" != "$TWSSC" ] ; then
-echo "\033[1mSnapshot \"$TWSSA\" available!\033[0m\nClick here to read latest changelogs."
+echo "\033[1mSnapshot \"$TWSSA\" available!\033[0m\nClick here to read the changelog."
 notify-send -i software-update-available-symbolic "Snapshot \"$TWSSA\" available!"
 fi
 EOF
