d743b7392427a8b36535f4d4d30fa9a2cabcc513
DarthWound <darthwound@live.com>
2018-01-27 16:45:02 +0100

    Update daynight.sh
    Make terminal schemes optional

--- rsrc/daynight.sh
+++ rsrc/daynight.sh
@@ -3,20 +3,21 @@
 gsk1="org.gnome.desktop.interface gtk-theme"
 gsk2="org.gnome.desktop.wm.preferences theme"
 gsc=$(gsettings get $gsk1)
-trpr=$(dconf read /org/gnome/terminal/legacy/profiles:/default | tr -d \')
+# Enable "dconf" lines if you want terminal colors customization
+#trpr=$(dconf read /org/gnome/terminal/legacy/profiles:/default | tr -d \')
 
 if [ "$gsc" == "'Adwaita'" ]
 then
 gsettings set $gsk1 'Adwaita-dark' && gsettings set $gsk2 'Adwaita-dark'
-dconf write /org/gnome/terminal/legacy/profiles:/:$trpr/bold-color "'#939597'"
-dconf write /org/gnome/terminal/legacy/profiles:/:$trpr/foreground-color "'#939597'"
-dconf write /org/gnome/terminal/legacy/profiles:/:$trpr/background-color "'#131517'"
+#dconf write /org/gnome/terminal/legacy/profiles:/:$trpr/bold-color "'#939597'"
+#dconf write /org/gnome/terminal/legacy/profiles:/:$trpr/foreground-color "'#939597'"
+#dconf write /org/gnome/terminal/legacy/profiles:/:$trpr/background-color "'#131517'"
 elif [ "$gsc" == "'Adwaita-dark'" ]
 then
 gsettings set $gsk1 'Adwaita' && gsettings set $gsk2 'Adwaita'
-dconf write /org/gnome/terminal/legacy/profiles:/:$trpr/bold-color "'#474543'"
-dconf write /org/gnome/terminal/legacy/profiles:/:$trpr/foreground-color "'#474543'"
-dconf write /org/gnome/terminal/legacy/profiles:/:$trpr/background-color "'#D7D5D3'"
+#dconf write /org/gnome/terminal/legacy/profiles:/:$trpr/bold-color "'#474543'"
+#dconf write /org/gnome/terminal/legacy/profiles:/:$trpr/foreground-color "'#474543'"
+#dconf write /org/gnome/terminal/legacy/profiles:/:$trpr/background-color "'#D7D5D3'"
 fi
 
 # Adapta (https://github.com/adapta-project/adapta-gtk-theme)
