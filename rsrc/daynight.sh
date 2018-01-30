bcf05900e064a3f3a5987d6db6b5411dd9f4d5ac
DarthWound <darthwound@live.com>
2018-01-30 05:02:40 +0100

    Update daynight.sh

--- rsrc/daynight.sh
+++ rsrc/daynight.sh
@@ -3,21 +3,16 @@
 gsk1="org.gnome.desktop.interface gtk-theme"
 gsk2="org.gnome.desktop.wm.preferences theme"
 gsc=$(gsettings get $gsk1)
-# Enable "dconf" lines if you want terminal colors customization
-#trpr=$(dconf read /org/gnome/terminal/legacy/profiles:/default | tr -d \')
 
+# Disable the two wget lines if you want to keep your current terminal theme
 if [ "$gsc" == "'Adwaita'" ]
 then
 gsettings set $gsk1 'Adwaita-dark' && gsettings set $gsk2 'Adwaita-dark'
-#dconf write /org/gnome/terminal/legacy/profiles:/:$trpr/bold-color "'#939597'"
-#dconf write /org/gnome/terminal/legacy/profiles:/:$trpr/foreground-color "'#939597'"
-#dconf write /org/gnome/terminal/legacy/profiles:/:$trpr/background-color "'#131517'"
+wget -qO- https://raw.githubusercontent.com/DarthWound/flatui-gnome-terminal/master/flatuidark-gt.sh | sh
 elif [ "$gsc" == "'Adwaita-dark'" ]
 then
 gsettings set $gsk1 'Adwaita' && gsettings set $gsk2 'Adwaita'
-#dconf write /org/gnome/terminal/legacy/profiles:/:$trpr/bold-color "'#474543'"
-#dconf write /org/gnome/terminal/legacy/profiles:/:$trpr/foreground-color "'#474543'"
-#dconf write /org/gnome/terminal/legacy/profiles:/:$trpr/background-color "'#D7D5D3'"
+wget -qO- https://raw.githubusercontent.com/DarthWound/flatui-gnome-terminal/master/flatuilight-gt.sh | sh
 fi
 
 # Adapta (https://github.com/adapta-project/adapta-gtk-theme)
